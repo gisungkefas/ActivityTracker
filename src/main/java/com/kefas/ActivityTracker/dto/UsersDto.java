416b3cee0cffed49b6ce46813a03cc0506d37c59
Kefas Gisung <gisungkefas@gmail.com>
2022-12-28 11:31:38 +0100

    complited and tested with postman

--- src/main/java/com/kefas/ActivityTracker/dto/UsersDto.java
+++ src/main/java/com/kefas/ActivityTracker/dto/UsersDto.java
@@ -1,26 +1,25 @@
 package com.kefas.ActivityTracker.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class UsersDto {
 
-    @NotBlank(message = "First Name should not be Empty")
+    @NotNull(message = "First Name should not be Empty")
     private String firstname;
 
-    @NotBlank(message = "Last Name should not be Empty")
+    @NotNull(message = "Last Name should not be Empty")
     private String lastname;
 
-    @NotBlank(message = "Email not Valid")
+    @NotNull(message = "Email not Valid")
     private String email;
 
-    @NotBlank(message = "Phone Number should not be Empty")
+    @NotNull(message = "Phone Number should not be Empty")
     private String phoneNumber;
 
-    @NotBlank(message = "Password should not be Empty")
+    @NotNull(message = "Password should not be Empty")
     private String password;
 }
